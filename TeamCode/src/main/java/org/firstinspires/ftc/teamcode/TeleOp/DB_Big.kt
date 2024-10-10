f3ed2777aed0f91f0197b35619f4c5f9320592ad
BetheBob1 <BetheBob082208@gmail.com>
2024-10-10 15:21:46 -0700

    Clean Up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DB_Big.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DB_Big.kt
@@ -4,63 +4,75 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.Servo
 
-@TeleOp(name = "DoernbecherTeleop", group="Basic Chassis")
+@TeleOp(name = "DB_BIG", group="AAAAAAAA")
 class DoernbecherTeleOp: LinearOpMode() {
     override fun runOpMode() {
         val motorFL = hardwareMap.get(DcMotor::class.java, "motorFL")
-        //reverse FR
         val motorFR = hardwareMap.get(DcMotor::class.java, "motorFR")
         motorFR.direction=DcMotorSimple.Direction.REVERSE
         val motorBL = hardwareMap.get(DcMotor::class.java, "motorBL")
         val motorBR = hardwareMap.get(DcMotor::class.java, "motorBR")
+
         val aimMotor = hardwareMap.get(DcMotor::class.java, "aimMotor")
+        aimMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         val airplaneServo = hardwareMap.get(Servo::class.java, "airplaneServo")
+
         val lowerServoPosition = 0.7
         val highestServoPosition = 1.0
         val speedDiv = 2.0
+        var armToggle = false
+
+        val currentGamepad1: Gamepad = Gamepad()
+        val currentGamepad2: Gamepad = Gamepad()
+        val previousGamepad1: Gamepad = Gamepad()
+        val previousGamepad2: Gamepad = Gamepad()
 
         telemetry.addData("Status", "Initialized")
         telemetry.update()
         waitForStart()
         while (opModeIsActive()) {
-            //leftX is intended for strafing
-            val leftX = gamepad1.left_stick_x
-            //leftY is intended for forward and backward
-            val leftY = -gamepad1.left_stick_y
-            //rigthX is indended for rotation
-            val rightX = gamepad1.right_stick_x
+            //GAMEPADS
+            previousGamepad1.copy(currentGamepad1)
+            previousGamepad2.copy(currentGamepad2)
+            currentGamepad1.copy(gamepad1)
+            currentGamepad2.copy(gamepad2)
 
+            //INPUT
+            val leftX = gamepad1.left_stick_x     //STRAFING
+            val leftY = -gamepad1.left_stick_y    //FORWARD + BACK
+            val rightX = gamepad1.right_stick_x   //ROTATE
+
+            //MOVE
             motorFL.power = (leftY + leftX + rightX) / speedDiv
             motorBL.power = (leftY - leftX + rightX) / speedDiv
             motorFR.power = (leftY - leftX - rightX) / speedDiv
             motorBR.power = (leftY + leftX - rightX) / speedDiv
 
-            telemetry.addData("Status", "Running")
-
-            telemetry.addLine("motorFL: ${motorFL.power} motorFR: ${motorFR.power} motorBL: ${motorBL.power} motorBR: ${motorBR.power} ")
-            if (gamepad1.dpad_down && aimMotor.currentPosition<300) {
-                telemetry.addLine("Running AIM Motor Original Position: ${aimMotor.currentPosition}")
+            //AIM MOTOR
+            if (currentGamepad1.dpad_down) {
                 aimMotor.power = 0.1
                 sleep(80)
                 aimMotor.power = 0.0
-                telemetry.addLine("Running AIM Motor New Position: ${aimMotor.currentPosition}")
-            }else if (gamepad1.dpad_up && aimMotor.currentPosition>-250) {
-                telemetry.addLine("Running AIM Motor Original Position: ${aimMotor.currentPosition}")
+            }else if (currentGamepad1.dpad_up) {
                 aimMotor.power =- 0.2
                 sleep(80)
                 aimMotor.power = 0.0
-                telemetry.addLine("Running AIM Motor New Position: ${aimMotor.currentPosition}")
             }
 
-            if(gamepad1.a){
-                airplaneServo.position = lowerServoPosition
-            } else if (gamepad1.b){
-                airplaneServo.position = highestServoPosition
-            }
+            //TOGGLES ON/OFF
+            if (currentGamepad1.b&& !previousGamepad1.b){ armToggle = !armToggle }
 
+            //AIRPLANE SERVO
+            if (armToggle){ airplaneServo.position = lowerServoPosition }
+            if (!armToggle){ airplaneServo.position = highestServoPosition }
 
+            //TELEMETRY
+            telemetry.addData("Status", "Running")
+            telemetry.addLine("motorFL: ${motorFL.power} motorFR: ${motorFR.power} motorBL: ${motorBL.power} motorBR: ${motorBR.power} ")
+            telemetry.addLine("AIM Motor Position: ${aimMotor.currentPosition}")
             telemetry.update()
             sleep(10)
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DB_Small.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DB_Small.kt
@@ -2,13 +2,12 @@ package org.firstinspires.ftc.teamcode.TeleOp
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.hardware.CRServo
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.Servo
 
-@TeleOp(name = "DB", group = "Among Us")
-class DB : LinearOpMode(){
+@TeleOp(name = "DB_SMALL", group = "AAAAAAA")
+class DB_Small : LinearOpMode(){
     override fun runOpMode() {
         val right = hardwareMap.get(DcMotor::class.java, "rightwheel")
         val left = hardwareMap.get(DcMotor::class.java, "leftwheel")
@@ -17,18 +16,17 @@ class DB : LinearOpMode(){
         val clawRotate = hardwareMap.get(Servo::class.java, "clawrotation")
         val claw = hardwareMap.get(Servo::class.java, "claw")
 
+        //VARIABLES
         val speedDiv = 3.0
-
         val startPos = .63
         val stopPos = .9
-
         val clawOpen = 0.3
         val clawClose = 0.55
+        var contPow = 0.0
 
+        //TOGGLES
         var leftArmToggle = false
         var rightArmToggle = false
-
-        var contPow = 0.0
 
         //GAME PADS
         val currentGamepad1: Gamepad = Gamepad()
@@ -53,44 +51,45 @@ class DB : LinearOpMode(){
             currentGamepad1.copy(gamepad1);
             currentGamepad2.copy(gamepad2);
 
+            //INPUTS
             val leftY = gamepad1.left_stick_y.toDouble()
             val rightx = gamepad1.right_stick_x.toDouble()
 
+            //MOVEMENT
             right.power = (leftY - rightx) / speedDiv
             left.power = (leftY + rightx) / speedDiv
 
             //Reset Purple
-            if (currentGamepad1.b&& !previousGamepad1.b){
-                leftArmToggle = !leftArmToggle
+            if (currentGamepad1.b&& !previousGamepad1.b){ leftArmToggle = !leftArmToggle }
+            if (currentGamepad1.x&& !previousGamepad1.x){ rightArmToggle = !rightArmToggle }
+
+            //MOVE SERVOS
+            if(!leftArmToggle) { launcher.position = startPos }  //Right Reset
+            if(leftArmToggle) { launcher.position = stopPos }    //Right Launch
+            if(rightArmToggle) { claw.position = clawOpen }      //Left Up
+            if(!rightArmToggle) { claw.position = clawClose }    //Left Down
+
+            //ROTATION
+            if (gamepad1.dpad_down && rotate.targetPosition < -10) {
+                rotate.targetPosition += 10
+                sleep(33)
             }
-            if (currentGamepad1.x&& !previousGamepad1.x){
-                rightArmToggle = !rightArmToggle
+            if (gamepad1.dpad_up && rotate.targetPosition > -630) {
+                rotate.targetPosition -= 10
+                sleep(33)
             }
-            //Reset Purple
-            if(!leftArmToggle) { launcher.position = startPos }
+            if (gamepad1.left_bumper && clawRotate.position < 0.45) {
+                clawRotate.position += 0.01
+                sleep(33)
+            }
+            if (gamepad1.right_bumper && clawRotate.position > 0.11) {
+                clawRotate.position -= 0.01
+                sleep(33)
+            }
 
-            //Launch Purple
-            if(leftArmToggle) { launcher.position = stopPos }
-
-            // Left Up
-            if(rightArmToggle) { claw.position = clawOpen }
-
-            //Left Down
-            if(!rightArmToggle) { claw.position = clawClose }
-
-            if (gamepad1.dpad_down && rotate.targetPosition < -10) {rotate.targetPosition += 10
-                sleep(33)}
-            if (gamepad1.dpad_up && rotate.targetPosition > -630) {rotate.targetPosition -= 10
-                sleep(33)}
-
-            if (gamepad1.left_bumper && clawRotate.position < 0.45) {clawRotate.position += 0.01
-                sleep(33)}
-            if (gamepad1.right_bumper && clawRotate.position > 0.11) {clawRotate.position -= 0.01
-            sleep(33)}
-
+            //TELEMETRY
             telemetry.addData("clawrotate", clawRotate.position)
             telemetry.addData("rotate", rotate.targetPosition)
-
             telemetry.update()
         }
     }

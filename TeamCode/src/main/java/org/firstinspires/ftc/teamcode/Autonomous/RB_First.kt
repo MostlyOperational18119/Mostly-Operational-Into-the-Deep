b868484c054735a2b2c190a2f71dc61658acd737
MostlyOperational18119 <64237204+MostlyOperational18119@users.noreply.github.com>
2024-11-01 12:04:28 -0700

    aa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RB_First.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RB_First.kt
@@ -49,18 +49,39 @@ class RB_First : DriveMethods() {
                 .splineTo(Vector2d(-58.26, -57.0), Math.toRadians(225.00))
                 .build()
 
-        val traj2: TrajectorySequence?=
+        val traj2: TrajectorySequence? =
             drive.trajectorySequenceBuilder(Pose2d(-58.26,-57.0, Math.toRadians(225.0)))
                 .setReversed(true)
-                .splineTo(Vector2d(-35.91, -31.73), Math.toRadians(90.00))
-                .splineTo(Vector2d(-45.21, -13.68), Math.toRadians(170.00))
-                .lineToConstantHeading(Vector2d(-52.81, -60.98))
-                .lineTo(Vector2d(-44.83, -15.96))
-                .lineTo(Vector2d(-54.90, -15.96))
-                .lineToConstantHeading(Vector2d(-60.03, -59.27))
-                .lineTo(Vector2d(-53.38, -15.96))
-                .splineTo(Vector2d(-62.69, -12.54), Math.toRadians(180.00))
-                .lineToConstantHeading(Vector2d(-63.07, -54.71))
+                .lineToLinearHeading(Pose2d(-20.0,-45.0, Math.toRadians(135.0)))
+                .setReversed(false)
+                .splineTo(Vector2d(-33.64, -37.09), Math.toRadians(135.20))
+                .build()
+
+        val traj3: TrajectorySequence? =
+            drive.trajectorySequenceBuilder(Pose2d(-33.64, -37.09, Math.toRadians(135.20)))
+                .lineTo(Vector2d(-39.5, -37.09))
+                .build()
+
+        val traj4: TrajectorySequence? =
+            drive.trajectorySequenceBuilder(Pose2d(-39.5, -37.09, Math.toRadians(135.00)))
+                .back(10.0)
+                .setReversed(false)
+                .splineTo(Vector2d(-58.26, -57.0), Math.toRadians(225.00))
+                .build()
+
+
+        val traj5: TrajectorySequence?=
+            drive.trajectorySequenceBuilder(Pose2d(-58.26,-57.0, Math.toRadians(225.0)))
+                .setReversed(true)
+//                .splineTo(Vector2d(-35.91, -31.73), Math.toRadians(90.00))
+//                .splineTo(Vector2d(-45.21, -13.68), Math.toRadians(170.00))
+//                .lineToConstantHeading(Vector2d(-52.81, -57.00))
+                .lineToLinearHeading(Pose2d(-37.83, -13.00, Math.toRadians(180.0)))
+                .lineTo(Vector2d(-56.00, -13.00))
+                .lineToConstantHeading(Vector2d(-60.03, -57.00))
+                .lineTo(Vector2d(-53.38, -14.00))
+                .splineTo(Vector2d(-64.00, -12.54), Math.toRadians(180.00))
+                .lineToConstantHeading(Vector2d(-64.00, -54.71))
                 .setReversed(false)
                 .waitSeconds(0.25)
                 .build()
@@ -78,25 +99,75 @@ class RB_First : DriveMethods() {
         if (isStopRequested) return
         telemetry.addLine("not stopped")
         telemetry.update()
+
+
         drive.followTrajectorySequence(traj1)
 
         slideMotor.targetPosition = -2000
-        slideMotor.power = -0.5
+        slideMotor.power = 1.0
         rotateMotor.targetPosition = 75
-        rotateMotor.power = 0.5
-        sleep(2000)
+        rotateMotor.power = 1.0
+        sleep(1000)
         rotateMotor.targetPosition = 200
-        rotateMotor.power = 0.5
-        sleep(1000)
+        rotateMotor.power = 1.0
+        sleep(1500)
         clawServo.power = -1.0
-        sleep(1000)
+        sleep(500)
         clawServo.power = 0.0
         rotateMotor.targetPosition = 100
-        rotateMotor.power = -0.5
-        sleep(2000)
+        rotateMotor.power = -1.0
+        sleep(600)
         slideMotor.targetPosition = 0
-        slideMotor.power = 0.5
+        slideMotor.power = 1.0
+        sleep(400)
+        rotateMotor.targetPosition = 1100
+        rotateMotor.power = 0.75
+
         drive.followTrajectorySequence(traj2)
+        sleep(300)
+        slideMotor.targetPosition = -500
+        slideMotor.power = 1.0
+        rotateServo!!.position = 0.25
+        sleep(300)
+
+        drive.followTrajectorySequence(traj3)
+        sleep(200)
+        clawServo.power = 1.0
+        rotateMotor.targetPosition = 1350
+        rotateMotor.power = 0.5
+        sleep(1000)
+        clawServo.power = 0.0
+        sleep(200)
+        rotateMotor.targetPosition = 75
+        rotateMotor.power = 0.75
+        rotateServo.position = 0.11
+        sleep(300)
+
+
+        drive.followTrajectorySequence(traj4)
+        slideMotor.targetPosition = -2000
+        slideMotor.power = 1.0
+        sleep(1000)
+        rotateMotor.targetPosition = 200
+        rotateMotor.power = 1.0
+        sleep(1500)
+        clawServo.power = -1.0
+        sleep(500)
+        clawServo.power = 0.0
+        rotateMotor.targetPosition = 100
+        rotateMotor.power = -1.0
+        sleep(600)
+        slideMotor.targetPosition = 0
+        slideMotor.power = 1.0
+        sleep(400)
+        rotateMotor.targetPosition = 1100
+        rotateMotor.power = 0.75
+
+        drive.followTrajectorySequence(traj5)
+        sleep(10000)
+
+
+
 
         while (opModeIsActive() && !isStopRequested) {
             drive.update()

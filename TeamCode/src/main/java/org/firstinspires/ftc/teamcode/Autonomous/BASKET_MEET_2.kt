376eadcc6d8df83286524721f7adf5340ec0a968
BetheBob1 <BetheBob082208@gmail.com>
2024-11-19 19:49:39 -0800

    basket

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BASKET_MEET_2.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BASKET_MEET_2.kt
@@ -21,25 +21,28 @@ class BASKET_MEET_2 : DriveMethods() {
         // Setup Odometry :)
         val drive = SampleMecanumDrive(hardwareMap)
 
-        val rotateMotor = hardwareMap.get(DcMotor::class.java, "motorRotate")
-        rotateMotor.targetPosition = 300
-        rotateMotor.power = 0.5
-        rotateMotor.mode = DcMotor.RunMode.RUN_TO_POSITION
-        rotateMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-
         val slideMotor = hardwareMap.get(DcMotor::class.java, "motorSlide")
         slideMotor.targetPosition = 0
         slideMotor.power = 0.0
         slideMotor.mode = DcMotor.RunMode.RUN_TO_POSITION
         slideMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
 
-        val clawServo = hardwareMap.get(CRServo::class.java, "clawServo")
-        val rotateServo = hardwareMap.get(Servo::class.java, "rotateServo")
+        val reachMotor = hardwareMap.get(DcMotor::class.java, "motoReach")
+        reachMotor.targetPosition = 0
+        reachMotor.power = 0.0
+        reachMotor.mode = DcMotor.RunMode.RUN_TO_POSITION
+        reachMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
 
-        rotateServo.position =  0.11
+        val clawServo = hardwareMap.get(Servo::class.java, "claw")
+        val transferServo = hardwareMap.get(Servo::class.java, "rotateServo")
+        val intakeServo = hardwareMap.get(CRServo::class.java, "claw")
 
-
-
+        val specimenUp = 2000
+        val specimenDown = 1900
+        val highBasket = 3000
+        val clawOpen = 0.5
+        val clawClose = 0.1
+        val extendReach = 2000
 
         drive.poseEstimate = Pose2d(-34.09, -63.19, Math.toRadians(-90.00))
         // Setup up the trajectory sequence (drive path)
@@ -52,37 +55,37 @@ class BASKET_MEET_2 : DriveMethods() {
                 .build()
 
         val traj2: TrajectorySequence? =
-            drive.trajectorySequenceBuilder(Pose2d(-4.4,-34.09, Math.toRadians(-90.0)))
+            drive.trajectorySequenceBuilder(Pose2d(-10.4,-34.01, Math.toRadians(-90.0)))
                 .setReversed(false)
                 .splineTo(Vector2d(-47.87, -39.89), Math.toRadians(90.00))
                 .build()
 
         val traj3: TrajectorySequence? =
-            drive.trajectorySequenceBuilder(Pose2d(-4.4,-34.09, Math.toRadians(-90.0)))
+            drive.trajectorySequenceBuilder(Pose2d(-47.87,-39.89, Math.toRadians(90.0)))
                 .setReversed(true)
                 .lineToLinearHeading(Pose2d(-55.0, -55.0, Math.toRadians(45.00)))
                 .build()
 
         val traj4: TrajectorySequence? =
-            drive.trajectorySequenceBuilder(Pose2d(-49.40, -42.89, Math.toRadians(-90.00)))
+            drive.trajectorySequenceBuilder(Pose2d(-55.0, -55.0, Math.toRadians(45.00)))
                 .back(2.0)
                 .build()
 
         val traj5: TrajectorySequence? =
             drive.trajectorySequenceBuilder(Pose2d(-57.26, -57.26, Math.toRadians(225.00)))
                 .setReversed(false)
-                .lineToLinearHeading(Pose2d(-57.75, -38.37, Math.toRadians(88.83)))
+                .lineToLinearHeading(Pose2d(-57.75, -38.37, Math.toRadians(90.0)))
                 .build()
 
 
         val traj6: TrajectorySequence?=
-            drive.trajectorySequenceBuilder(Pose2d(-59.55,-43.09, Math.toRadians(-90.0)))
+            drive.trajectorySequenceBuilder(Pose2d(-57.75,-38.37, Math.toRadians(90.0)))
                 .setReversed(true)
                 .lineToLinearHeading(Pose2d(-55.0, -55.0, Math.toRadians(45.00)))
                 .build()
 
         val traj7: TrajectorySequence?=
-            drive.trajectorySequenceBuilder(Pose2d(-59.55,-43.09, Math.toRadians(-90.0)))
+            drive.trajectorySequenceBuilder(Pose2d(-55.0,-55.0, Math.toRadians(45.0)))
                 .back(2.0)
                 .setReversed(false)
                 .build()
@@ -102,73 +105,79 @@ class BASKET_MEET_2 : DriveMethods() {
         telemetry.update()
 
 
+        //PLACE SPECIMEN ON HIGH BAR
+        slideMotor.targetPosition = specimenUp
+        slideMotor.power = 1.0
+
         drive.followTrajectorySequence(traj1)
 
-        slideMotor.targetPosition = -2000
+        slideMotor.targetPosition = specimenDown
         slideMotor.power = 1.0
-        rotateMotor.targetPosition = 75
-        rotateMotor.power = 1.0
-        sleep(1000)
-        rotateMotor.targetPosition = 200
-        rotateMotor.power = 1.0
-        sleep(1500)
-        clawServo.power = -1.0
-        sleep(500)
-        clawServo.power = 0.0
-        rotateMotor.targetPosition = 100
-        rotateMotor.power = -1.0
-        sleep(600)
+        sleep(200)
+        clawServo.position = clawOpen
+        sleep(200)
+        slideMotor.targetPosition = specimenUp+50
+        slideMotor.power = 1.0
+        sleep(100)
         slideMotor.targetPosition = 0
-        slideMotor.power = 1.0
-        sleep(400)
-        rotateMotor.targetPosition = 1100
-        rotateMotor.power = 0.75
+        slideMotor.power = 0.5
 
+        //GO TO AND PICK UP FIRST SAMPLE
         drive.followTrajectorySequence(traj2)
-        sleep(300)
-        slideMotor.targetPosition = -500
-        slideMotor.power = 1.0
-        rotateServo!!.position = 0.25
-        sleep(300)
 
-        drive.followTrajectorySequence(traj3)
-        sleep(200)
-        clawServo.power = 1.0
-        rotateMotor.targetPosition = 1350
-        rotateMotor.power = 0.5
+        reachMotor.targetPosition = extendReach
+        reachMotor.power = 1.0
+        sleep(1500)
+        intakeServo.power = 1.0
         sleep(1000)
-        clawServo.power = 0.0
-        sleep(200)
-        rotateMotor.targetPosition = 75
-        rotateMotor.power = 0.75
-        rotateServo.position = 0.11
-        sleep(300)
+        intakeServo.power = 0.0
+        reachMotor.targetPosition = 0
+        reachMotor.power = 1.0
+        //TransferSystemGoes Here
 
+        //MOVE TO HIGH BASKET AND PLACE SAMPLE
+        drive.followTrajectorySequence(traj3)
+
+        slideMotor.targetPosition = highBasket
+        slideMotor.power = 1.0
+        sleep(1000)
 
         drive.followTrajectorySequence(traj4)
-        slideMotor.targetPosition = -2000
+
+        clawServo.position = clawOpen
+        sleep(100)
+        slideMotor.targetPosition = 0
+        slideMotor.power = 0.5
+        sleep(500)
+
+
+        //MOVE TO 2nd PIXEL AND PICK UP
+        drive.followTrajectorySequence(traj5)
+
+        reachMotor.targetPosition = extendReach
+        reachMotor.power = 1.0
+        sleep(1500)
+        intakeServo.power = 1.0
+        sleep(1000)
+        intakeServo.power = 0.0
+        reachMotor.targetPosition = 0
+        reachMotor.power = 1.0
+        //TransferSystemGoes Here
+
+        //MOVE TO HIGH BASKET AND PLACE SECOND SAMPLE
+        drive.followTrajectorySequence(traj6)
+
+        slideMotor.targetPosition = highBasket
         slideMotor.power = 1.0
         sleep(1000)
-        rotateMotor.targetPosition = 200
-        rotateMotor.power = 1.0
-        sleep(2000)
-        clawServo.power = -1.0
-        sleep(1500)
-        clawServo.power = 0.0
-        rotateMotor.targetPosition = 100
-        rotateMotor.power = -1.0
-        sleep(600)
+
+        drive.followTrajectorySequence(traj7)
+
+        clawServo.position = clawOpen
+        sleep(100)
         slideMotor.targetPosition = 0
-        slideMotor.power = 1.0
-        sleep(400)
-        rotateMotor.targetPosition = 1100
-        rotateMotor.power = 0.75
-
-        drive.followTrajectorySequence(traj5)
-        sleep(10000)
-
-
-
+        slideMotor.power = 0.5
+        sleep(500)
 
         while (opModeIsActive() && !isStopRequested) {
             drive.update()

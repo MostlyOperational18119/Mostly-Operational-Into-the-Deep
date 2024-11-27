79283d928b426d342eb4419b294eac4a9fa16b28
SaarangShaikh <saarangpshaikh@gmail.com>
2024-11-27 12:30:16 -0800

    baby bot teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BabyBotAuto.kt
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.autonomous
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+@Autonomous(name = "BabyBotAuto")
+class BabyBotAuto : LinearOpMode() {
+    override fun runOpMode() {
+
+    }
+}

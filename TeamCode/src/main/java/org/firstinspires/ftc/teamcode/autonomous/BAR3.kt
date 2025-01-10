5bf09eafba26056fb6049b0409a224a76d61581b
MostlyOperational18119 <64237204+MostlyOperational18119@users.noreply.github.com>
2025-01-09 16:17:10 -0800

    added last block push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BAR3.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BAR3.kt
@@ -73,6 +73,13 @@ class BAR3 : Methods() {
                 .lineToConstantHeading(Vector2d(55.85, -61.55))
                 .build()
 
+        val push: TrajectorySequence =
+            drive!!.trajectorySequenceBuilder(Pose2d(55.85, -61.55, Math.toRadians(-90.00)))
+                .lineToConstantHeading(Vector2d(55.85, -13.0))
+                .lineToConstantHeading(Vector2d(70.0, -13.0))
+                .lineToConstantHeading(Vector2d(70.0, -61.55))
+                .build()
+
         waitForStart()
 
         //START
@@ -135,6 +142,8 @@ class BAR3 : Methods() {
         //END
         verticalSlideTo(0, 1.0)
         drive!!.followTrajectorySequence(end)
+        sleep(100)
+        drive!!.followTrajectorySequence(push)
 
         while (opModeIsActive() && !isStopRequested) { drive!!.update() }
         PoseStorage.currentPose = drive!!.poseEstimate

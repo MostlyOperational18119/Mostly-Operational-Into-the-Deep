59b9ccbda08f6300309450723265049f6643266c
Jesse He <heqiqi@hotmail.com>
2025-02-19 16:02:55 -0800

    Color sensor test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ColorSensorTest.kt
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.teleop
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.ColorSensor
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor
+
+
+@TeleOp
+class ColorSensorTest : LinearOpMode() {
+    // Define a variable for our color sensor
+    var color_sensor: ColorSensor? = null
+
+    override fun runOpMode() {
+        // Get the color sensor from hardwareMap
+        color_sensor = hardwareMap.get(ColorSensor::class.java, "colorSensor")
+        // Wait for the Play button to be pressed
+        waitForStart()
+        // While the OpMode is running, update the telemetry values.
+        while (opModeIsActive()) {
+            telemetry.addData("Light Detected", (color_sensor as OpticalDistanceSensor).lightDetected)
+            telemetry.addData("Red", (color_sensor as ColorSensor).red())
+            telemetry.addData("Green", (color_sensor as ColorSensor).green())
+            telemetry.addData("Blue", (color_sensor as ColorSensor).blue())
+            telemetry.update()
+        }
+    }
+}

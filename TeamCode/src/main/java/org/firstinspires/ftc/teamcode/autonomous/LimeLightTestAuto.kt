7bb4044bb97cb3004d59a858548996eba5470ec1
TheComputerToucher <damien.brown15@outlook.com>
2025-02-20 17:35:50 -0800

    Ruin LLTestAuto, add logging to auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/LimeLightTestAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/LimeLightTestAuto.kt
@@ -1,18 +1,14 @@
 package org.firstinspires.ftc.teamcode.autonomous
 
-import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.qualcomm.hardware.limelightvision.Limelight3A
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import org.firstinspires.ftc.teamcode.Methods
-import org.firstinspires.ftc.teamcode.drive.advanced.SampleMecanumDriveCancelable
 
 @Autonomous(name = "LimeLight Test Autonomous")
-@Disabled
 class LimeLightTestAuto : Methods() {
     override fun runOpMode() {
-        val drive = SampleMecanumDriveCancelable(hardwareMap)
+//        val drive = SampleMecanumDriveCancelable(hardwareMap)
         val limelight = hardwareMap.get(Limelight3A::class.java, "limelight")
 
         telemetry.addLine("Init done")
@@ -40,9 +36,11 @@ class LimeLightTestAuto : Methods() {
 
         sleep(1000) // Just to be safe
 
+        /*
         val llResults = limelight.latestResult
         var lMove = 0.0 // Add offsets from camera space to centered robot here
         var fMove = 0.0
+
 
         if (llResults.colorResults.isNotEmpty()) {
             val colorResult = llResults.colorResults[0]
@@ -51,6 +49,8 @@ class LimeLightTestAuto : Methods() {
             val inchSamplePose = colorResult.targetPoseRobotSpace.position.toUnit(DistanceUnit.INCH)
             lMove += inchSamplePose.x
             fMove += inchSamplePose.z
+
+            telemetry.addLine("Result: (${inchSamplePose.x}, ${inchSamplePose.y}, ${inchSamplePose.z})")
         }
 
         // Start Auto Movement
@@ -58,8 +58,28 @@ class LimeLightTestAuto : Methods() {
             .strafeLeft(lMove)
             .forward(fMove)
             .build()
+         */
 
-        drive.followTrajectorySequence(trajectorySequence)
+        while(opModeIsActive()) {
+            val llResults = limelight.latestResult
+
+            if (llResults.colorResults.isNotEmpty()) {
+                llResults.colorResults.forEach {
+                    val inchSamplePose = it.targetPoseCameraSpace.position.toUnit(DistanceUnit.INCH)
+
+                    telemetry.addLine("Result: (${inchSamplePose.x}, ${inchSamplePose.y}, ${inchSamplePose.z})")
+                    telemetry.update()
+                }
+            } else {
+                telemetry.addLine("No Result")
+                telemetry.update()
+            }
+
+            sleep(50)
+
+        }
+
+//        drive.followTrajectorySequence(trajectorySequence)
 
         // End Auto Movement
     }
